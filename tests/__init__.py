fbb3db17ab423f8672c7468e70b10ee66c4d163d
Nadia Bouaicha <nadia.bouaicha@esprit.tn>
2025-03-02 13:55:53 +0100

    Fix formatting for tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,3 @@ import os
 
 # Ajoute le dossier racine du projet au PYTHONPATH pour que les tests trouvent les modules
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
-
